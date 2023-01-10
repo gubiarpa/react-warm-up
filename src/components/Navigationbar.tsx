5ad3989af45f507fbfdb03eacb2eb4d3f6429fbc
Billy Arredondo <billy.arredondo@pucp.pe>
2023-01-10 02:39:38 -0500

    create pages
    - Navigationbar component

---
+++ src/components/Navigationbar.tsx
@@ -0,0 +1,11 @@
+import { Container, Navbar } from "react-bootstrap";
+
+const Navigationbar = () => {
+	return (
+		<Navbar>
+			<Container>Navbar</Container>
+		</Navbar>
+	);
+};
+
+export default Navigationbar;
