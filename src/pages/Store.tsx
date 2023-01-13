3e04c2d119456f61c1c82062194db920a3aec43d
Billy Arredondo <billy.arredondo@pucp.pe>
2023-01-13 04:13:04 -0500

    adding barrel file on pages

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,19 @@
-const Store = () => {
-	return <div>Store</div>;
-};
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
-export default Store;
+export const Store = () => {
+	const quantity = 0;
+	return (
+		<>
+			<h1>Store</h1>
+			<Row md={2} xs={1} lg={3} className="g-3">
+				{storeItems.map((item) => (
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
+};
